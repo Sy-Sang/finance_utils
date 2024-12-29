0179846641a4df2092f1bc635fe410cb286f9fd4
martin9le <sangsiyuan@me.com>
2024-12-29 23:54:33 +0800

    2024/12/29 周日 23:54:32.22, from home

---
+++ payoff/basic/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Sy,Sang"
+__version__ = ""
+__license__ = "GPLv3"
+__maintainer__ = "Sy, Sang"
+__email__ = "martin9le@163.com"
+__status__ = "Development"
+__credits__ = []
+__date__ = ""
+__copyright__ = ""
+__all__ = []
---
+++ payoff/basic/base.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Sy,Sang"
+__version__ = ""
+__license__ = "GPLv3"
+__maintainer__ = "Sy, Sang"
+__email__ = "martin9le@163.com"
+__status__ = "Development"
+__credits__ = []
+__date__ = ""
+__copyright__ = ""
+
+# 系统模块
+import copy
+import pickle
+import json
+from typing import Union, Self
+from collections import namedtuple
+
+# 项目模块
+
+# 外部模块
+import numpy
+
+# 代码块
+
+class PayOff:
+    def __init__(self, strike_point:float):
+        self.strike_point = strike_point
+
+    def __call__(self, x, *args, **kwargs):
+        return x - self.strike_point
+
+
+if __name__ == "__main__":
+    pass
