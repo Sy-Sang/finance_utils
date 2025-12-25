b1e04a85ebe49efdfd8c4c9def5d55084d267cd1
martin9le <sangsiyuan@me.com>
2025-12-25 09:32:59 +0800

    2025/12/25 周四  9:32:47.11 - from home

---
+++ financial_instruments/ontology/temporal_profile.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""时间对象"""
+
+__author__ = "Sy,Sang"
+__version__ = ""
+__license__ = "GPLv3"
+__maintainer__ = "Sy, Sang"
+__email__ = "martin9le@163.com"
+__status__ = "Development"
+__credits__ = []
+__date__ = ""
+__copyright__ = ""
+
+# 系统模块
+import copy
+import pickle
+import json
+from typing import Union, Self
+from collections import namedtuple
+
+# 项目模块
+
+# 外部模块
+import numpy
+
+# 代码块
+
+
+if __name__ == "__main__":
+    pass
